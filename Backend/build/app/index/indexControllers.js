48c857583ea607cb9371e0dc4c4c24b96e07e7a1
JuanDa237 <juandavid200301@hotmail.com>
2020-10-13 13:00:36 -0500

    I change all.

--- Backend/build/app/index/indexControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexController = void 0;
-class IndexController {
-    index(request, response) {
-        return response.json({ message: "Welcome to my api, the documentation is in folder Sources/Postman." });
-    }
-}
-exports.indexController = new IndexController();
