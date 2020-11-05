5c2a860115217b624c9438a9c2b0ddd6b0b9ac08
JuanDa237 <juandavid200301@hotmail.com>
2020-11-05 09:25:18 -0500

    I update somethings in the frontend

--- frontend/src/app/modules/main/landing/components/home-nav/home-nav.component.ts
+++ frontend/src/app/modules/main/landing/components/home-nav/home-nav.component.ts
@@ -4,17 +4,35 @@ import { filter } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-home-nav',
-	templateUrl: './home-nav.component.html'
+	templateUrl: './home-nav.component.html',
+	styleUrls: ['./home-nav.component.scss']
 })
 export class HomeNavComponent {
-	constructor(private router: Router) {}
+	public addActive: boolean[];
 
-	to(id: string) {
+	constructor(private router: Router) {
+		this.addActive = new Array<boolean>(4);
+
+		for (var i = 0; i < this.addActive.length; i++) {
+			this.addActive[i] = false;
+		}
+	}
+
+	to(id: string, index: number) {
+		//Add active class
+		for (var i = 0; i < this.addActive.length; i++) {
+			this.addActive[i] = false;
+		}
+		this.addActive[index] = true;
+
+		//Scroll to the section
 		var element = document.getElementById(id);
 		element?.scrollIntoView({ behavior: 'smooth' });
 
 		this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
-			if (!element) document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+			if (!element) {
+				document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+			}
 		});
 	}
 }
