1b4abc9abeb8e5fe2098d784c36b73e5c4f63d9c
JuanDa237 <juandavid200301@hotmail.com>
2020-11-06 09:51:21 -0500

    I change the log in.

--- frontend/src/app/modules/main/landing/containers/sign-in/sign-in.component.ts
+++ frontend/src/app/modules/main/landing/containers/sign-in/sign-in.component.ts
@@ -9,7 +9,8 @@ import { LogInUser } from '../../models/index';
 
 @Component({
 	selector: 'app-sign-in',
-	templateUrl: './sign-in.component.html'
+	templateUrl: './sign-in.component.html',
+	styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
 	public user: LogInUser;
