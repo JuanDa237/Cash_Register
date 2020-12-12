2a920ebb556a3de812ce11679e6fecf0e606ddfe
JuanDa237 <juandavid200301@hotmail.com>
2020-12-11 23:42:48 -0500

    - I add a animation in sidenav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+	imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HttpClientModule],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
--- frontend/src/app/modules/main/navigation/components/side-nav-item/side-nav-item.component.ts
+++ frontend/src/app/modules/main/navigation/components/side-nav-item/side-nav-item.component.ts
@@ -3,10 +3,20 @@ import { Component, Input } from '@angular/core';
 // Models
 import { SideNavItem } from '@modules/main/navigation/models';
 
+//Animations
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
 	selector: 'app-side-nav-item',
 	templateUrl: './side-nav-item.component.html',
-	styleUrls: ['./side-nav-item.component.scss']
+	styleUrls: ['./side-nav-item.component.scss'],
+	animations: [
+		trigger('fade', [
+			state('void', style({ opacity: 1 })),
+			transition(':enter', [style({ opacity: 0 }), animate('0.5s ease-out')]),
+			transition(':leave', [animate('0.5s ease-in', style({ opacity: 0 }))])
+		])
+	]
 })
 export class SideNavItemComponent {
 	@Input()
