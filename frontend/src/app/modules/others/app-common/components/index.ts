9fd0c79f83270610948a081e7ee7f27d7884afe1
JuanDa237 <juandavid200301@hotmail.com>
2021-04-09 23:37:47 -0500

    - Change invalid form messages

--- frontend/src/app/modules/others/app-common/components/index.ts
+++ frontend/src/app/modules/others/app-common/components/index.ts
@@ -1,16 +1,9 @@
 import { ModalComponent } from './modal/modal.component';
 import { LoadingComponent } from './loading/loading.component';
 import { TableComponent } from './table/table.component';
-import { CardComponentComponent } from './card/card-component.component';
 
-export const components = [
-	ModalComponent,
-	LoadingComponent,
-	TableComponent,
-	CardComponentComponent
-];
+export const components = [ModalComponent, LoadingComponent, TableComponent];
 
 export * from './modal/modal.component';
 export * from './loading/loading.component';
 export * from './table/table.component';
-export * from './card/card-component.component';
